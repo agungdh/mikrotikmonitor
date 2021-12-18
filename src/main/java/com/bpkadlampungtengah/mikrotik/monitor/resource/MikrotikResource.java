87c3be280379a614d173cc7a547cdc125e4af3ae
AgungDH <agungdh@live.com>
2021-12-18 10:28:07 +0700

    add interface monitoring optimizing query

--- src/main/java/com/bpkadlampungtengah/mikrotik/monitor/resource/MikrotikResource.java
+++ src/main/java/com/bpkadlampungtengah/mikrotik/monitor/resource/MikrotikResource.java
@@ -19,6 +19,26 @@ public class MikrotikResource {
     @Autowired
     Environment env;
 
+    @GetMapping("/iface/indihome")
+    public ResponseEntity<Map<String, String>> indihome() throws MikrotikApiException {
+        ApiConnection con = ApiConnection.connect(env.getProperty("mikrotik.host"));
+        con.login(env.getProperty("mikrotik.username"),env.getProperty("mikrotik.password"));
+
+        List<Map<String, String>> rs = con.execute("/interface/monitor-traffic interface=ether10 once");
+
+        return ResponseEntity.ok().body(rs.get(0));
+    }
+
+    @GetMapping("/iface/icon")
+    public ResponseEntity<Map<String, String>> icon() throws MikrotikApiException {
+        ApiConnection con = ApiConnection.connect(env.getProperty("mikrotik.host"));
+        con.login(env.getProperty("mikrotik.username"),env.getProperty("mikrotik.password"));
+
+        List<Map<String, String>> rs = con.execute("/interface/monitor-traffic interface=ether1 once");
+
+        return ResponseEntity.ok().body(rs.get(0));
+    }
+
     @GetMapping("/ppps")
     public ResponseEntity<List<Map<String, String>>> ppps() throws MikrotikApiException {
         ApiConnection con = ApiConnection.connect(env.getProperty("mikrotik.host"));
@@ -36,24 +56,11 @@ public class MikrotikResource {
         ApiConnection con = ApiConnection.connect(env.getProperty("mikrotik.host"));
         con.login(env.getProperty("mikrotik.username"),env.getProperty("mikrotik.password"));
 
-        List<Map<String, String>> rs = con.execute("/queue/simple/print");
-
-        Map<String, String> data = null;
-
-        for (int i = 1; i <= rs.size(); i++) {
-            String tempVar = "<" + request.getService() + "-" + request.getName() + ">";
-            if (rs.get(i - 1).get("name").equals(tempVar)) {
-                data = rs.get(i - 1);
-            }
-        }
+        Map<String, String> rs = con.execute("/queue/simple/print where name=\"<" + request.getService() + "-" + request.getName() + ">\"").get(0);
 
         con.close();
 
-        if (data == null) {
-            return ResponseEntity.badRequest().body(data);
-        } else {
-            return ResponseEntity.ok().body(data);
-        }
+        return ResponseEntity.ok().body(rs);
     }
 }
 
