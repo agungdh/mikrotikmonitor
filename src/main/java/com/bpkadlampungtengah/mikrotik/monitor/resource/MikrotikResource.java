0e7e28f2712353275fb0bb8aa725415f3de5a936
AgungDH <agungdh@live.com>
2021-12-18 10:41:18 +0700

    response

--- src/main/java/com/bpkadlampungtengah/mikrotik/monitor/resource/MikrotikResource.java
+++ src/main/java/com/bpkadlampungtengah/mikrotik/monitor/resource/MikrotikResource.java
@@ -1,6 +1,8 @@
 package com.bpkadlampungtengah.mikrotik.monitor.resource;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.legrange.mikrotik.ApiConnection;
 import me.legrange.mikrotik.MikrotikApiException;
@@ -20,23 +22,33 @@ public class MikrotikResource {
     Environment env;
 
     @GetMapping("/iface/indihome")
-    public ResponseEntity<Map<String, String>> indihome() throws MikrotikApiException {
+    public ResponseEntity<InterfaceMonitoring> indihome() throws MikrotikApiException {
         ApiConnection con = ApiConnection.connect(env.getProperty("mikrotik.host"));
         con.login(env.getProperty("mikrotik.username"),env.getProperty("mikrotik.password"));
 
         List<Map<String, String>> rs = con.execute("/interface/monitor-traffic interface=ether10 once");
 
-        return ResponseEntity.ok().body(rs.get(0));
+        InterfaceMonitoring data = new InterfaceMonitoring(
+                Long.valueOf(rs.get(0).get("tx-bits-per-second")),
+                Long.valueOf(rs.get(0).get("rx-bits-per-second"))
+        );
+
+        return ResponseEntity.ok().body(data);
     }
 
     @GetMapping("/iface/icon")
-    public ResponseEntity<Map<String, String>> icon() throws MikrotikApiException {
+    public ResponseEntity<InterfaceMonitoring> icon() throws MikrotikApiException {
         ApiConnection con = ApiConnection.connect(env.getProperty("mikrotik.host"));
         con.login(env.getProperty("mikrotik.username"),env.getProperty("mikrotik.password"));
 
         List<Map<String, String>> rs = con.execute("/interface/monitor-traffic interface=ether1 once");
 
-        return ResponseEntity.ok().body(rs.get(0));
+        InterfaceMonitoring data = new InterfaceMonitoring(
+                Long.valueOf(rs.get(0).get("tx-bits-per-second")),
+                Long.valueOf(rs.get(0).get("rx-bits-per-second"))
+        );
+
+        return ResponseEntity.ok().body(data);
     }
 
     @GetMapping("/ppps")
@@ -62,6 +74,14 @@ public class MikrotikResource {
 
         return ResponseEntity.ok().body(rs);
     }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class InterfaceMonitoring {
+    private Long upload;
+    private Long download;
 }
 
 @Data
