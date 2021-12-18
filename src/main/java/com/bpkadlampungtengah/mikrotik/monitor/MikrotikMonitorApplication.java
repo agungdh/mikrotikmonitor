b530f3912cb7273c5fe669d484cb32fcddf216c5
AgungDH <agungdh@live.com>
2021-12-18 12:06:09 +0700

    add cors allow all

--- src/main/java/com/bpkadlampungtengah/mikrotik/monitor/MikrotikMonitorApplication.java
+++ src/main/java/com/bpkadlampungtengah/mikrotik/monitor/MikrotikMonitorApplication.java
@@ -2,6 +2,10 @@ package com.bpkadlampungtengah.mikrotik.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MikrotikMonitorApplication {
@@ -10,4 +14,13 @@ public class MikrotikMonitorApplication {
 		SpringApplication.run(MikrotikMonitorApplication.class, args);
 	}
 
+	@Configuration
+	@EnableWebMvc
+	public class WebConfig implements WebMvcConfigurer {
+
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**");
+		}
+	}
 }
